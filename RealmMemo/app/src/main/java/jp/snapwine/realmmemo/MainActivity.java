cac14a570cd2b9f1e3b7c34263b83ea2ec46e305
TomokoKondo <to.2a721@gmail.com>
2016-03-02 16:43:53 +0900

    RealmMemoプロジェクト追加
    jarファイル追加済み

---
+++ RealmMemo/app/src/main/java/jp/snapwine/realmmemo/MainActivity.java
@@ -0,0 +1,48 @@
+package jp.snapwine.realmmemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+
+        // メニューの要素を追加して取得
+        MenuItem actionItem = menu.add("Action Button");
+
+        // SHOW_AS_ACTION_IF_ROOM:余裕があれば表示
+        actionItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+        // アイコンを設定
+        actionItem.setIcon(android.R.drawable.ic_menu_add);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- okandroid/app/src/main/java/com/example/kondoutomoko/okandroid/MainActivity.java
+++ okandroid/app/src/main/java/com/example/kondoutomoko/okandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kondoutomoko.okandroid;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,16 +9,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     private Button buttonMaru, buttonBatu;
-    private TextView textInfo;
+    private TextView textInfo, textQuestion;
+
+    private Thread thread;
+    private boolean isRunning = false;
+    private Handler handler;
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textQuestion = (TextView)findViewById(R.id.text_question);
         textInfo = (TextView)findViewById(R.id.text_info);
         buttonMaru = (Button)findViewById(R.id.btn_maru);
         buttonBatu = (Button)findViewById(R.id.btn_batu);
@@ -36,6 +44,38 @@ public class MainActivity extends AppCompatActivity {
                 setButtonEnable(false);
             }
         });
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+        handler = new Handler();
+    }
+    @Override
+    protected void onDestroy() {
+        isRunning = false;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            long nextTime = System.currentTimeMillis() + 2000;
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    counter++;
+                    if(counter==2){
+                    textQuestion.setText("Mondai2");
+                    textInfo.setText("cotae");
+                   }
+                }
+            });
+
+            try {
+                Thread.sleep(nextTime - System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void setButtonEnable(boolean isEnable) {
